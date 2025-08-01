3861d9ad895f6384daffb7b522e2b2d41b180485
timiwhosa <timiwhosa@gmail.com>
2025-08-01 11:24:18 +0100

    fixing ghpages deploy ish, atempt 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repo = "nibi"
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // enables static export (replaces `next export`)
+  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
